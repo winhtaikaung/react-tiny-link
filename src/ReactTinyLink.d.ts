4b50e7f66d5eaa1f3a731830ce8dddce347aeb06
win <winhtaikaung28@gmail.com>
2019-09-08 22:12:37 +0800

    Merge conflict solved

--- src/ReactTinyLink.d.ts
+++ src/ReactTinyLink.d.ts
@@ -1,15 +1,11 @@
-import * as React from 'react'
-import { ReactTinyLinkType, IReactTinyLinkProps } from './ReactTinyLinkTypes'
-export declare const ScrapperWraper: (
-  url: string,
-  httpClient: any,
-  defaultMedia: string[],
-) => Promise<{
-  title: any
-  url: any
-  description: any
-  type: ReactTinyLinkType
-  video: any[]
-  image: any[]
-}>
-export declare const ReactTinyLink: React.FC<IReactTinyLinkProps>
+import * as React from 'react';
+import { ReactTinyLinkType, IReactTinyLinkProps } from './ReactTinyLinkTypes';
+export declare const ScrapperWraper: (url: string, httpClient: any, defaultMedia: string[]) => Promise<{
+    title: any;
+    url: any;
+    description: any;
+    type: ReactTinyLinkType;
+    video: any[];
+    image: any[];
+}>;
+export declare const ReactTinyLink: React.FC<IReactTinyLinkProps>;
