2f1973162e73cc6ec0daf9cd70cad6807110026e
sndsabin <sndsabin@gmail.com>
2020-03-24 23:27:59 +0545

    change type to boolean

--- src/components/CardMedia.d.ts
+++ src/components/CardMedia.d.ts
@@ -2,6 +2,6 @@ declare const CardMedia: ({ data, cardSize, autoPlay, onlySecure }: {
     data: any;
     cardSize: any;
     autoPlay: any;
-    onlySecure: any;
+    onlySecure: boolean;
 }) => JSX.Element;
 export default CardMedia;
